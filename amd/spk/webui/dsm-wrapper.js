cbb352355ea325f1f71d8190befb2c0d4f1f4303
PeterSuh-Q3 <dante9000@gmail.com>
2026-09-02 23:44:17 +0900

    fix: use DSM ExtJS floating AppWindow implementation

--- amd/spk/webui/dsm-wrapper.js
+++ amd/spk/webui/dsm-wrapper.js
@@ -1,1 +1,3 @@
-require(['/webman/modules/appWindow/AppWindow.js'],function(AppWindow){AppWindow.register({id:'AmdGpuMonitor.AppWindow',title:'AMD GPU Monitor',width:760,height:620,minWidth:520,minHeight:480,url:'/webman/3rdparty/SynoAmdGpuMonitor/index.html'});});
+Ext.ns('AmdGpuMonitor');
+Ext.define('AmdGpuMonitor.AppInstance',{extend:'SYNO.SDS.AppInstance',appWindowName:'AmdGpuMonitor.AppWindow'});
+Ext.define('AmdGpuMonitor.AppWindow',{extend:'SYNO.SDS.AppWindow',constructor:function(config){this.callParent([Ext.apply({resizable:true,maximizable:true,minimizable:true,width:760,height:620,minWidth:520,minHeight:480,layout:'fit',border:false,items:[{xtype:'box',autoEl:{tag:'iframe',src:'/webman/3rdparty/SynoAmdGpuMonitor/index.html',frameborder:'0',style:'width:100%;height:100%;border:none;'}}]},config)]);}});
