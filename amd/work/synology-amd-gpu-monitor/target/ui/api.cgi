55b45a17ab55453ddb984035f842d2878f2ed447
PeterSuh-Q3 <dante9000@gmail.com>
2026-09-03 00:31:35 +0900

    chore: remove generated build outputs from repository

--- amd/work/synology-amd-gpu-monitor/target/ui/api.cgi
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-printf 'Content-Type: application/json\r\n\r\n'
-exec /var/packages/synology-amd-gpu-monitor/target/bin/amd-gpu-monitor
--- amd/work/synology-amd-gpu-monitor/target/ui/dsm-wrapper.js
+++
@@ -1,3 +0,0 @@
-Ext.ns('AmdGpuMonitor');
-Ext.define('AmdGpuMonitor.AppInstance',{extend:'SYNO.SDS.AppInstance',appWindowName:'AmdGpuMonitor.AppWindow'});
-Ext.define('AmdGpuMonitor.AppWindow',{extend:'SYNO.SDS.AppWindow',constructor:function(config){this.callParent([Ext.apply({resizable:true,maximizable:true,minimizable:true,width:760,height:620,minWidth:520,minHeight:480,layout:'fit',border:false,items:[{xtype:'box',autoEl:{tag:'iframe',src:'/webman/3rdparty/SynoAmdGpuMonitor/index.html',frameborder:'0',style:'width:100%;height:100%;border:none;'}}]},config)]);}});
